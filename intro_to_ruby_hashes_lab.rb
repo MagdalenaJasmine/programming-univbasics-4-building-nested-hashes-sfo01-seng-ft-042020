6fc5fe43762a193dd7f0ee09fe6ec9cda8eedd6b
Magdalena Cooper <magdalenacooper92@gmail.com>
2020-04-02 04:24:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,5 +34,11 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+base_hash={
+   :railroads => {
+     :pieces => 4,
+     :rent_in_dollars => {:once_piece_owned =},
+     :names => {}
+   }
+ }
 end
