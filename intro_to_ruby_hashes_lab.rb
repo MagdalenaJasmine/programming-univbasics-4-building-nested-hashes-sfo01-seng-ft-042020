0bdccb0177713c698741759c1940b4b3ddb35bce
Magdalena Cooper <magdalenacooper92@gmail.com>
2020-04-02 04:34:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,7 @@ def monopoly_with_fourth_tier
 base_hash={
    :railroads => {
      :pieces => 4,
-     :rent_in_dollars => {once_piece_owned:25, 
+     :rent_in_dollars => {once_piece_owned: 25, 
        :two_pieces_owned => 50, 
        :three_pieces_owned => 100, 
        :four_pieces_owned => 200
